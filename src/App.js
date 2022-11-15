0fead62900c9bf5b77474a04ed1112fc8806e9fb
Ainis Taranda <taranda.ainis@gmail.com>
2022-11-15 14:04:44 -0500

    useState for switching languages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import English from "./lang/en.json";
+import Spanish from "./lang/es.json";
+import { useState } from "react";
+import "./App.css";
+
+const locale = navigator.language || "en";
+
+const _lang = locale === "es" ? Spanish : English;
 
 function App() {
+  const [lang, setLang] = useState(_lang);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="button">
+        <button onClick={() => setLang(English)}>EN</button>
+        <button onClick={() => setLang(Spanish)}>ES</button>
+      </div>
+      <h1>{lang["app.header"]}</h1>
+      <p>{lang["app.subhead"]}</p>
+      <a>{lang["app.cta"]}</a>
     </div>
   );
 }
